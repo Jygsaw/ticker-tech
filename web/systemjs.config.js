ef40ccc99ed307d1e73bb568e197183aa38bce51
Justin Cheung <jygsaw@gmail.com>
2016-06-18 14:42:26 -0700

    [NEW] Include Angular

--- web/systemjs.config.js
+++ web/systemjs.config.js
@@ -1,11 +1,40 @@
 (function () {
   let map = {
+    "@angular": "node_modules/@angular",
     "rxjs": "node_modules/rxjs"
   };
 
   let packages = {
     "rxjs": { defaultExtension: "js" }
   };
+
+  let ngPackageNames = [
+    "common",
+    "compiler",
+    "core",
+    "http",
+    "platform-browser",
+    "platform-browser-dynamic",
+    "router",
+    "router-deprecated",
+    "upgrade"
+  ];
+
+  // Individual files (~300 requests):
+  function packIndex(pkgName) {
+    packages["@angular/" + pkgName] = { main: "index.js", defaultExtension: "js" };
+  }
+
+  // Bundled (~40 requests):
+  function packUmd(pkgName) {
+    packages["@angular/" + pkgName] = { main: "/bundles/" + pkgName + ".umd.js", defaultExtension: "js" };
+  }
+
+  // Most environments should use UMD; some (Karma) need the individual index files
+  let setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+
+  // Add Angular package entries
+  ngPackageNames.forEach(setPackageConfig);
 
   System.config({
     map: map,
