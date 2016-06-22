115985a3b03978e088a0636d15c696b5365a4714
Justin Cheung <jygsaw@gmail.com>
2016-06-22 15:45:27 -0700

    [NEW] Create and integrate HomeComponent

--- web/site/app.routes.ts
+++ web/site/app.routes.ts
@@ -1,6 +1,14 @@
 import { provideRouter, RouterConfig } from "@angular/router";
 
+import { PublicRoutes } from "./public/public.routes";
+
 export const routes: RouterConfig = [
+  {
+    path: "",
+    redirectTo: "/home",
+    terminal: true,
+  },
+  ...PublicRoutes,
 ];
 
 export const APP_ROUTER_PROVIDERS = [
