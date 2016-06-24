62fa03486c6a9649736e36cf71a2a0806154ac1f
Justin Cheung <jygsaw@gmail.com>
2016-06-24 09:39:23 -0700

    [NEW] Create and integrate TermsComponent

--- web/site/public/public.routes.ts
+++ web/site/public/public.routes.ts
@@ -1,10 +1,15 @@
 import { RouterConfig } from "@angular/router";
 
 import { HomeComponent } from "./home/home.component";
+import { TermsComponent } from "./terms/terms.component";
 
 export const PublicRoutes: RouterConfig = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'terms',
+    component: TermsComponent,
   }
 ];
