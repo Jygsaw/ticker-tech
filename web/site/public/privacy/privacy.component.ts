c168fc8abc5e58195bd0e5d849a1a003b0b74d28
Justin Cheung <jygsaw@gmail.com>
2016-06-24 10:58:43 -0700

    [NEW] Create and integrate PrivacyComponent

---
+++ web/site/public/privacy/privacy.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  templateUrl: "privacy.component.html",
+})
+export class PrivacyComponent {
+};
--- web/site/public/public.routes.ts
+++ web/site/public/public.routes.ts
@@ -1,12 +1,17 @@
 import { RouterConfig } from "@angular/router";
 
 import { HomeComponent } from "./home/home.component";
+import { PrivacyComponent } from "./privacy/privacy.component";
 import { TermsComponent } from "./terms/terms.component";
 
 export const PublicRoutes: RouterConfig = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'privacy',
+    component: PrivacyComponent,
   },
   {
     path: 'terms',
