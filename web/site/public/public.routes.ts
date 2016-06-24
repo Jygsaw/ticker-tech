36f432b58d7d0a46ff268e39eabe94f6d62ffd07
Justin Cheung <jygsaw@gmail.com>
2016-06-24 11:04:50 -0700

    [NEW] Create and integrate AboutComponent

--- web/site/public/public.routes.ts
+++ web/site/public/public.routes.ts
@@ -1,10 +1,15 @@
 import { RouterConfig } from "@angular/router";
 
+import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
 import { PrivacyComponent } from "./privacy/privacy.component";
 import { TermsComponent } from "./terms/terms.component";
 
 export const PublicRoutes: RouterConfig = [
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
   {
     path: 'home',
     component: HomeComponent,
