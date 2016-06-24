36bc53f69e50616c82396694caf41463cdcfc06f
Justin Cheung <jygsaw@gmail.com>
2016-06-24 11:08:52 -0700

    [NEW] Create and integrate ContactComponent

--- web/site/public/public.routes.ts
+++ web/site/public/public.routes.ts
@@ -1,6 +1,7 @@
 import { RouterConfig } from "@angular/router";
 
 import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
 import { HomeComponent } from "./home/home.component";
 import { PrivacyComponent } from "./privacy/privacy.component";
 import { TermsComponent } from "./terms/terms.component";
@@ -9,6 +10,10 @@ export const PublicRoutes: RouterConfig = [
   {
     path: 'about',
     component: AboutComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   },
   {
     path: 'home',
