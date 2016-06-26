a6d6551f8233d34e6f39a69411efe6eff671e6e0
Justin Cheung <jygsaw@gmail.com>
2016-06-25 22:13:46 -0700

    [NEW] Create and integrate BalancesComponent

--- web/site/account/account.routes.ts
+++ web/site/account/account.routes.ts
@@ -3,11 +3,22 @@ import { RouterConfig } from "@angular/router";
 import { AuthGuard } from "guards/auth.guard";
 
 import { AccountComponent } from "./account.component";
+import { BalancesComponent } from "./balances/balances.component";
 
 export const AccountRoutes: RouterConfig = [
   {
     path: "account",
     component: AccountComponent,
     canActivate: [ AuthGuard ],
+    children: [
+      {
+        path: "",
+        component: BalancesComponent,
+      },
+      {
+        path: "balances",
+        component: BalancesComponent,
+      },
+    ],
   }
 ];
---
+++ web/site/account/balances/balances.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  templateUrl: "balances.component.html",
+})
+export class BalancesComponent {
+};
