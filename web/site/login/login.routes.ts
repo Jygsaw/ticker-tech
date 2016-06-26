41857a4443aca12e9061045895ee25778072f566
Justin Cheung <jygsaw@gmail.com>
2016-06-26 11:59:44 -0700

    [NEW] Create and integrate ForgotPasswordComponent

--- web/site/login/login.routes.ts
+++ web/site/login/login.routes.ts
@@ -1,8 +1,13 @@
 import { RouterConfig } from "@angular/router";
 
+import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
 import { SignInComponent } from "./sign-in/sign-in.component";
 
 export const LoginRoutes: RouterConfig = [
+  {
+    path: "forgot-password",
+    component: ForgotPasswordComponent,
+  },
   {
     path: "sign-in",
     component: SignInComponent,
--- web/site/login/sign-in/sign-in.component.ts
+++ web/site/login/sign-in/sign-in.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
-import { Router } from "@angular/router";
+import { ROUTER_DIRECTIVES, Router } from "@angular/router";
 
 import { AuthService } from "services/auth.service";
 
 @Component({
   moduleId: module.id,
   templateUrl: "sign-in.component.html",
+  directives: [ ROUTER_DIRECTIVES ],
 })
 export class SignInComponent {
   constructor(private authService: AuthService, private router: Router) {}
