069c657d0ffa670d80b4c5c1bef590837e844eca
Justin Cheung <jygsaw@gmail.com>
2016-06-28 11:44:31 -0700

    [NEW] Create and integrate StockPositionsComponent

---
+++ web/site/stocks/stock-positions/stock-positions.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  templateUrl: "stock-positions.component.html",
+})
+export class StockPositionsComponent {
+};
--- web/site/stocks/stocks.routes.ts
+++ web/site/stocks/stocks.routes.ts
@@ -3,11 +3,22 @@ import { RouterConfig } from "@angular/router";
 import { AuthGuard } from "guards/auth.guard";
 
 import { StocksComponent } from "./stocks.component";
+import { StockPositionsComponent } from "./stock-positions/stock-positions.component";
 
 export const StocksRoutes: RouterConfig = [
   {
     path: "stocks",
     component: StocksComponent,
     canActivate: [ AuthGuard ],
+    children: [
+      {
+        path: "",
+        component: StockPositionsComponent,
+      },
+      {
+        path: "stock-positions",
+        component: StockPositionsComponent,
+      },
+    ],
   }
 ];
