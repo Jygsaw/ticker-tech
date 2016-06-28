1a7552bd3e4ebedf7d2f373b9740e751783cf683
Justin Cheung <jygsaw@gmail.com>
2016-06-28 11:58:46 -0700

    [NEW] Create and integrate ProfileComponent

--- web/site/settings/settings.routes.ts
+++ web/site/settings/settings.routes.ts
@@ -3,11 +3,22 @@ import { RouterConfig } from "@angular/router";
 import { AuthGuard } from "guards/auth.guard";
 
 import { SettingsComponent } from "./settings.component";
+import { ProfileComponent } from "./profile/profile.component";
 
 export const SettingsRoutes: RouterConfig = [
   {
     path: "settings",
     component: SettingsComponent,
     canActivate: [ AuthGuard ],
+    children: [
+      {
+        path: "",
+        component: ProfileComponent,
+      },
+      {
+        path: "profile",
+        component: ProfileComponent,
+      },
+    ],
   }
 ];
