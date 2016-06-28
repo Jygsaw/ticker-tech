dce0fdd63cfe4e7b9e9e83f27e9016d9e0ae49e5
Justin Cheung <jygsaw@gmail.com>
2016-06-28 11:49:45 -0700

    [NEW] Create and integrate FundPositionsComponent

---
+++ web/site/funds/fund-positions/fund-positions.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  templateUrl: "fund-positions.component.html",
+})
+export class FundPositionsComponent {
+};
--- web/site/funds/funds.routes.ts
+++ web/site/funds/funds.routes.ts
@@ -3,11 +3,22 @@ import { RouterConfig } from "@angular/router";
 import { AuthGuard } from "guards/auth.guard";
 
 import { FundsComponent } from "./funds.component";
+import { FundPositionsComponent } from "./fund-positions/fund-positions.component";
 
 export const FundsRoutes: RouterConfig = [
   {
     path: "funds",
     component: FundsComponent,
     canActivate: [ AuthGuard ],
+    children: [
+      {
+        path: "",
+        component: FundPositionsComponent,
+      },
+      {
+        path: "fund-positions",
+        component: FundPositionsComponent,
+      },
+    ],
   }
 ];
