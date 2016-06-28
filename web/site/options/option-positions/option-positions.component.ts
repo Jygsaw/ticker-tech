149b01dd275e5d1af1e5a0be9da965c499b5f3a9
Justin Cheung <jygsaw@gmail.com>
2016-06-28 11:53:53 -0700

    [NEW] Create and integrate OptionPositionsComponent

---
+++ web/site/options/option-positions/option-positions.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  templateUrl: "option-positions.component.html",
+})
+export class OptionPositionsComponent {
+};
--- web/site/options/options.routes.ts
+++ web/site/options/options.routes.ts
@@ -3,11 +3,22 @@ import { RouterConfig } from "@angular/router";
 import { AuthGuard } from "guards/auth.guard";
 
 import { OptionsComponent } from "./options.component";
+import { OptionPositionsComponent } from  "./option-positions/option-positions.component";
 
 export const OptionsRoutes: RouterConfig = [
   {
     path: "options",
     component: OptionsComponent,
     canActivate: [ AuthGuard ],
+    children: [
+      {
+        path: "",
+        component: OptionPositionsComponent,
+      },
+      {
+        path: "option-positions",
+        component: OptionPositionsComponent,
+      },
+    ],
   }
 ];
