eb246039af39f5f12550238c387928e8d05d3c87
Justin Cheung <jygsaw@gmail.com>
2016-06-29 10:22:38 -0700

    [NEW] Create and integrate MainMenuComponent

--- web/site/app.component.ts
+++ web/site/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
 
+import { MainMenuComponent } from "./main-menu/main-menu.component";
+
 @Component({
   moduleId: module.id,
   selector: "app",
   templateUrl: "app.component.html",
+  directives: [ MainMenuComponent ],
 })
 export class AppComponent {
   title = "TickerTech";
---
+++ web/site/main-menu/main-menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  selector: "main-menu",
+  templateUrl: "main-menu.component.html",
+})
+export class MainMenuComponent {
+};
