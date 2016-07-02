0a4011e696f8077ae2c74298ad5b5ca1279c8068
Justin Cheung <jygsaw@gmail.com>
2016-07-01 23:57:51 -0700

    [NEW] Implement auth service request to api auth endpoint

--- web/shared/services/auth.service.ts
+++ web/shared/services/auth.service.ts
@@ -1,14 +1,47 @@
 import { Injectable } from "@angular/core";
+import { Headers, Http, RequestOptions } from "@angular/http";
+import "rxjs/add/operator/toPromise";
+
+import { User } from "classes/user";
+import { promiseError } from "utils/utils";
+
+// TODO: move endpoint to central config file
+// TODO: enable https
+const endpoint = "../../api/auth";
 
 @Injectable()
 export class AuthService {
   isLoggedIn: boolean = false;
+  accessToken: string = null;
+  user: User = null;
 
-  login() {
-    return new Promise((resolve, reject) => {
-      this.isLoggedIn = true;
-      resolve();
+  constructor(private http: Http) {}
+
+  login(username: string = null, password: string = null) {
+    let credentials = JSON.stringify({
+      "username": username,
+      "password": password,
     });
+    let headers = new Headers({ "Content-Type": "application/json" });
+    let options = new RequestOptions({ "headers": headers });
+
+    // TODO: review promise return implementation
+    return this.http
+      .post(endpoint, credentials, options)
+      .toPromise()
+      .then(response => {
+        let reply = response.json();
+
+        if (reply.status === "success") {
+          this.isLoggedIn = true;
+          this.accessToken = reply.data.accessToken;
+          this.user = reply.data.user;
+          return Promise.resolve<string>(reply.status);
+        } else {
+          return Promise.reject<any>(reply);
+        }
+      })
+      .catch(promiseError);
   }
 
   logout() {
