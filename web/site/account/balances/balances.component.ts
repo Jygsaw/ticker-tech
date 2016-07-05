ff5bb9f0f5a4e45e2b567313b18d5a02c2cc3023
Justin Cheung <jygsaw@gmail.com>
2016-07-05 01:32:52 -0700

    [NEW] Implement cash balance on Account Balances page

--- web/site/account/balances/balances.component.ts
+++ web/site/account/balances/balances.component.ts
@@ -1,8 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import { Balances } from "classes/balances";
+
+import { AccountService } from "services/account.service";
+import { AuthService } from "services/auth.service";
 
 @Component({
   moduleId: module.id,
   templateUrl: "balances.component.html",
 })
 export class BalancesComponent {
+  private balances: Balances = {};
+  private balancesFailed: boolean = false;
+
+  constructor(private authService: AuthService, private accountService: AccountService) {}
+
+  ngOnInit() {
+    this.accountService
+      .getBalances([ "cash" ])
+      .then((data) => {
+        Object.assign(this.balances, data);
+      })
+      .catch((err: any) => {
+        console.error(err);
+        this.balancesFailed = true;
+      });
+  }
 };
