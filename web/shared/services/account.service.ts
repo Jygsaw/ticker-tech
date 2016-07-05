8b61fcc4db62f5973baf3331938deb9f24f73daf
Justin Cheung <jygsaw@gmail.com>
2016-07-05 01:30:30 -0700

    [NEW] Create AccountService

---
+++ web/shared/services/account.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from "@angular/core";
+import { Headers, Http, RequestOptions } from "@angular/http";
+import "rxjs/add/operator/toPromise";
+
+import { Balances } from "classes/balances";
+import { promiseError } from "utils/utils";
+
+import { AuthService } from "services/auth.service";
+
+// TODO: move endpoint to central config file
+// TODO: enable https
+const endpoint = "../../api/account/balances";
+
+@Injectable()
+export class AccountService {
+  private balances: Balances = {};
+
+  constructor(private authService: AuthService, private http: Http) {}
+
+  getBalances(types: string[]) {
+    let data = JSON.stringify({
+      "userId": this.authService.user.id,
+      "types": types,
+    });
+    let headers = new Headers({ "Content-Type": "application/json" });
+    let options = new RequestOptions({ "headers": headers });
+
+    // TODO: review promise return implementation
+    return this.http
+      .post(endpoint, data, options)
+      .toPromise()
+      .then(response => {
+        let reply = response.json();
+
+        if (reply.status === "success") {
+          Object.assign(this.balances, reply.data.balances);
+          return Promise.resolve<Balances>(this.balances);
+        } else {
+          return Promise.reject<any>(reply);
+        }
+      })
+      .catch(promiseError);
+  }
+};
