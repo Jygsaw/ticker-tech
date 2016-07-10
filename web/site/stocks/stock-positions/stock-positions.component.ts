3adea26aecab30ce7e51b767ca82810aa607c2ae
Justin Cheung <jygsaw@gmail.com>
2016-07-09 19:43:24 -0700

    [NEW] Implement Stock Positions display

--- web/site/stocks/stock-positions/stock-positions.component.ts
+++ web/site/stocks/stock-positions/stock-positions.component.ts
@@ -1,8 +1,44 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import { Positions } from "classes/positions";
+
+import { AccountService } from "services/account.service";
 
 @Component({
   moduleId: module.id,
   templateUrl: "stock-positions.component.html",
 })
 export class StockPositionsComponent {
+  private positions: Positions = {};
+  private stocksValue: number = null;
+  private positionsFailed: boolean = false;
+
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit() {
+    // fetch positions
+    this.accountService
+      .getPositions([ "stocks" ])
+      .then((data: Positions) => {
+        // update component
+        Object.assign(this.positions, data);
+      })
+      .then(() => {
+        // fetch market prices
+        // TODO contact 3rd party for price data
+        this.positions.stocks.map((position) => position.price = 5);
+      })
+      .then(() => {
+        // calc total value
+        this.stocksValue = 0;
+        this.positions.stocks.map((position) => {
+          position.value = position.quantity * position.price;
+          this.stocksValue += position.value;
+        });
+      })
+      .catch((err: any) => {
+        console.error(err);
+        this.positionsFailed = true;
+      });
+  }
 };
