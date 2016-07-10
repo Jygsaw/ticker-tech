77ce211159312c0fe27d28329726d7bee3481478
Justin Cheung <jygsaw@gmail.com>
2016-07-09 21:19:32 -0700

    [NEW] Implement API account positions route

--- api/account/account.ts
+++ api/account/account.ts
@@ -6,6 +6,8 @@ let router: express.Router = express.Router();
 
 router.use("/balances", require("./balances/balances").default);
 
+router.use("/positions", require("./positions/positions").default);
+
 router.use((req, res) => res.sendStatus(200));
 
 export default router;
