289a678e933da8eea64eb18021d1d5f9344108eb
Justin Cheung <jygsaw@gmail.com>
2016-07-10 02:57:25 -0700

    [NEW] Implement AccountService getPositions()

--- web/shared/services/account.service.ts
+++ web/shared/services/account.service.ts
@@ -3,6 +3,7 @@ import { Headers, Http, RequestOptions } from "@angular/http";
 import "rxjs/add/operator/toPromise";
 
 import { Balances } from "classes/balances";
+import { Positions } from "classes/positions";
 import { promiseError } from "utils/utils";
 
 import { AuthService } from "services/auth.service";
@@ -11,11 +12,13 @@ import { AuthService } from "services/auth.service";
 // TODO: enable https
 const endpoints = {
   "balances": "../../api/account/balances",
+  "positions": "../../api/account/positions",
 };
 
 @Injectable()
 export class AccountService {
   private balances: Balances = {};
+  private positions: Positions = {};
 
   constructor(private authService: AuthService, private http: Http) {}
 
@@ -37,6 +40,31 @@ export class AccountService {
         if (reply.status === "success") {
           Object.assign(this.balances, reply.data.balances);
           return Promise.resolve<Balances>(this.balances);
+        } else {
+          return Promise.reject<any>(reply);
+        }
+      })
+      .catch(promiseError);
+  }
+
+  getPositions(types: string[]) {
+    let data = JSON.stringify({
+      "userId": this.authService.user.id,
+      "types": types,
+    });
+    let headers = new Headers({ "Content-Type": "application/json" });
+    let options = new RequestOptions({ "headers": headers });
+
+    // TODO: review promise return implementation
+    return this.http
+      .post(endpoints.positions, data, options)
+      .toPromise()
+      .then(response => {
+        let reply = response.json();
+
+        if (reply.status === "success") {
+          Object.assign(this.positions, reply.data.positions);
+          return Promise.resolve<Positions>(this.positions);
         } else {
           return Promise.reject<any>(reply);
         }
