25222e8cd00db35860e65c0b4af763850f82a176
Justin Cheung <jygsaw@gmail.com>
2016-07-20 08:02:23 -0700

    [NEW] Implement Account Orders page

--- web/site/account/orders/orders.component.ts
+++ web/site/account/orders/orders.component.ts
@@ -1,8 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import { Order } from "classes-common/order";
+
+import { AccountService } from "services/account.service";
+
+import { promiseError } from "utils/utils";
 
 @Component({
   moduleId: module.id,
   templateUrl: "orders.component.html",
 })
 export class OrdersComponent {
+  private orders: Order[] = [];
+  private fetchFailed: boolean = false;
+
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit() {
+    // fetch orders
+    this.accountService
+      .getOrders()
+      .then(data => this.orders = data)
+      .catch(promiseError)
+      .catch(() => this.fetchFailed = true);
+  }
 };
