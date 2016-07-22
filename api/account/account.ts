692fe07d1730dc24861b2fa18acd52abc5eebc2c
Justin Cheung <jygsaw@gmail.com>
2016-07-22 13:18:54 -0700

    [RFG] Remove obsolete code

--- api/account/account.ts
+++ api/account/account.ts
@@ -4,8 +4,6 @@ import * as express from "express";
 
 let router: express.Router = express.Router();
 
-router.use("/balances", require("./balances/balances").default);
-
 router.use((req, res) => res.sendStatus(200));
 
 export default router;
--- web/shared/classes/balances.ts
+++
@@ -1,3 +0,0 @@
-export class Balances {
-  cash?: number;
-}
