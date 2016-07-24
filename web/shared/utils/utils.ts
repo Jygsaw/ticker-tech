0b4faca6bf57a92d694642c3a82f2d460f6ebf5e
Justin Cheung <jygsaw@gmail.com>
2016-07-24 11:01:47 -0700

    [NEW] Create cookie utils

--- web/shared/utils/utils.ts
+++ web/shared/utils/utils.ts
@@ -1,4 +1,22 @@
+// TODO implement cookie anti-tampering
+/***** Cookie Funcs: begin *****/
+export function setCookie(key: string, val: string) {
+  document.cookie = `${key}=${val};path=/`;
+}
+
+export function getCookie(key: string) {
+  let pattern = String.raw `(?:(?:^|.*;\s*)${key}\s*\=\s*([^;]*).*$)|^.*$`;
+  return document.cookie.replace(new RegExp(pattern), "$1");
+}
+
+export function deleteCookie(key: string) {
+  document.cookie = `${key}=;path=/;expires=` + new Date().toUTCString();
+}
+/***** Cookie Funcs: end *****/
+
+/***** Promise Funcs: begin *****/
 export function promiseError(error: any) {
   console.error("An error occurred:", error);
   return Promise.reject(error.message || error);
 }
+/***** Promise Funcs: end *****/
