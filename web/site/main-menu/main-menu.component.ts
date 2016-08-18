37bd3e1a65e3a8ab90b081726c557f8cfc66625c
Justin Cheung <jygsaw@gmail.com>
2016-08-18 16:40:52 -0700

    [NEW] Add MainMenuComponent styling

--- web/site/main-menu/main-menu.component.ts
+++ web/site/main-menu/main-menu.component.ts
@@ -1,9 +1,13 @@
 import { Component } from "@angular/core";
 
+import { AuthService} from "services/auth.service";
+
 @Component({
   moduleId: module.id,
   selector: "main-menu",
   templateUrl: "main-menu.component.html",
+  styleUrls: [ "main-menu.component.css" ],
 })
 export class MainMenuComponent {
+  constructor(private authService: AuthService) {}
 };
