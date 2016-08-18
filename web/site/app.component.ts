efa17f37a41c032f817fba5bfff2a1a548261f79
Justin Cheung <jygsaw@gmail.com>
2016-08-18 15:43:17 -0700

    [NEW] Add AppComponent styling

--- web/site/app.component.ts
+++ web/site/app.component.ts
@@ -9,6 +9,7 @@ import { getCookie } from "utils/utils";
   moduleId: module.id,
   selector: "app",
   templateUrl: "app.component.html",
+  styleUrls: [ "app.component.css" ],
 })
 export class AppComponent {
   private title: string = "TickerTech";
